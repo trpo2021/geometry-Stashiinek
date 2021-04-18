8f4debb01828b124562674d8c6fedc0d4ff79458
Stashiinek <metrikfeed@gmail.com>
2021-04-18 18:49:36 +0700

    Create geometry.h

---
+++ geometry.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct circle{
+  float x;
+  float y;
+  float radius;
+
+  float per;
+  float sq;
+};
+
+typedef struct circle circle;
+
+void Check(char ch[], int n, circle *ln1);
+
+void Perimeter(circle *ring);
+
+void Square(circle *ring);
